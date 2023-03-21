f467a74f4e66c1bf5cc29d20dd9b95efe66a4334
Vladimir Dementyev <palkan@hey.com>
2023-03-21 16:07:02 -0400

    fix: write eval_gemfile once

--- template/_config.rb
+++ template/_config.rb
@@ -6,9 +6,6 @@ file "gemfiles/rubocop.gemfile", <%= code "gemfiles/rubocop.gemfile" %>
 
 in_root do
   if File.file?("Gemfile")
-    # inject line in the end of the file with Thor
-    last_non_emoty_line = File.readlines("Gemfile").reverse.find { |line| !line.strip.empty? }
-
-    inject_into_file "Gemfile", %(\ngroup :development do\n  eval_gemfile "gemfiles/rubocop.gemfile"\nend), after: last_non_emoty_line
+    file "Gemfile", File.read("Gemfile") + %(\ngroup :development do\n  eval_gemfile "gemfiles/rubocop.gemfile"\nend\n), force: true
   end
 end
